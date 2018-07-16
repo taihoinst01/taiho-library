3aecd8438888d53b0664158979f4bc686a66c19d
Sinook Kang <sokang337@gmail.com>
2018-07-16 16:46:11 +0900

    firtst Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="taihoLibraryChatBot.WebApiApplication" Language="C#" %>
---
+++ Models/ConfList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace taihoLibraryChatBot.Models
+{
+    public class ConfList
+    {
+        public string cnfType;
+        public string cnfNm;
+        public string cnfValue;
+    }
+}
