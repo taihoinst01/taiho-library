1a476cafe1639ff48ec3d094f3d49dc247236ba3
Sinook Kang <sokang337@gmail.com>
2018-07-16 11:07:21 +0900

    firstCommit

---
+++ Models/RootObject.cs
@@ -0,0 +1,13 @@
+﻿namespace taihoLibraryChatBot.Models
+{
+	using Newtonsoft.Json;
+	using System;
+	using System.Collections.Generic;
+
+	[Serializable]
+	public class RootObject
+	{
+		public List<Search> items { get; set; }
+		public int display { get; set; }
+	}
+}
